a34a3befe858254d38e1a179e1a9d4eed9d003f8
Slavqaaa <s89148025956@yandex.ru>
2021-11-21 02:16:21 +0900

    Сделан Импорт полностью, Главный виджет переработан, остался Экспорт, ошибки и подключение

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pandas as pd
+from datetime import date
+import numpy as np
 import sys
 from PyQt5 import uic
-import pymysql
+import mysql.connector
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QLineEdit, QFileDialog, QInputDialog, QMessageBox
-from PyQt5.QtWidgets import QInputDialog
+from PyQt5.QtWidgets import QInputDialog, QTableWidgetItem
 from design.main_design import Ui_MainWindow as MainDesignPy
 from design.setting import Ui_MainWindow as SettingDesign
 
@@ -20,26 +22,114 @@ class SettingMainR(QWidget, SettingDesign):
 class LoadStress(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('design/load stress.ui', self)  # Загружаем дизайн
-        self.pushButton.clicked.connect(self.run)
-        # Обратите внимание: имя элемента такое же как в QTDesigner
+        uic.loadUi('design/load stress.ui', self)
+        self.update_data_table()
 
-    def run(self):
-        self.label.setText("OK")
-        # Имя элемента совпадает с objectName в QTDesigner
+        # Заполняем табл
+        self.addStrees.clicked.connect(self.add_strees_in_db)
+        self.connection.close()
+
+    def add_strees_in_db(self):
+        self.file = open("data/setting.txt", encoding="UTF-8")
+        data = self.file.readlines()
+        self.file.close()
+        self.connection = mysql.connector.connect(
+            host=data[0][:-1],
+            user=data[1][:-1],
+            password=data[2][:-1],
+            database=data[3],
+        )
+        if self.stressData.text() != '':
+            data = [self.stressData.text(),
+                    self.yeardata.text(),
+                    self.dataQuartal.text(),
+                    self.datapart.text(),
+                    self.dataWay.text(),
+                    str(date.today().strftime("%d:%B:%Y"))]
+            query = f'''INSERT INTO stress_table(stress_data, year, quarter, part, way, data) values("{data[0]}", "{data[1]}", "{data[2]}", "{data[3]}", "{data[4]}", "{data[5]}")'''
+            cursor = self.connection.cursor()
+            cursor.execute(query)
+            self.connection.commit()
+            self.connection.close()
+            self.update_data_table()
+
+    def update_data_table(self):
+        self.file = open("data/setting.txt", encoding="UTF-8")
+        data = self.file.readlines()
+        self.file.close()
+        self.connection = mysql.connector.connect(
+            host=data[0][:-1],
+            user=data[1][:-1],
+            password=data[2][:-1],
+            database=data[3],
+        )
+
+        query = '''DESCRIBE stress_table'''
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.tableWidget.setRowCount(0)
+        rows_table = []
+        for element in result:
+            rows_table.append(element[0])
+            print(f"'{element[0]}'", end=' ')
+        self.tableWidget.setHorizontalHeaderLabels(rows_table)
+        print(result)
+        query = '''Select * From stress_table'''
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for i, row in enumerate(result):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
 
 
-class MainWearDesign(QWidget, MainDesignPy):
+class MainWearDesign(QMainWindow):
     def __init__(self):
         super().__init__()
         # Вызываем метод для загрузки интерфейса из класса Ui_MainWindow,
         # остальное без изменений
-        self.setupUi(self)
+        uic.loadUi('design/Main.ui', self)
+        self.update_table_widget()
         self.settingfile = ''
         self.actionSetting.triggered.connect(self.setting_open)
         self.actionOpenStress.triggered.connect(self.strees_open)
         self.actionImport.triggered.connect(self.import_xlxs)
         self.actionExport.triggered.connect(self.export_xlxs)
+
+
+    def update_table_widget(self):
+        self.file = open("data/setting.txt", encoding="UTF-8")
+        data = self.file.readlines()
+        self.file.close()
+        self.connection = mysql.connector.connect(
+            host=data[0][:-1],
+            user=data[1][:-1],
+            password=data[2][:-1],
+            database=data[3],
+        )
+
+        query = '''DESCRIBE resault_iznos'''
+        cursor = self.connection.cursor(buffered=True)
+        cursor.execute(query)
+        result = cursor.fetchall()
+        self.tableWidget.setColumnCount(17)
+        self.tableWidget.setRowCount(0)
+        rows_table = []
+        for element in result:
+            rows_table.append(element[0])
+            print(f"'{element[0]}'", end=' ')
+        self.tableWidget.setHorizontalHeaderLabels(rows_table)
+        print(result)
+        query = '''Select * From resault_iznos'''
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for i, row in enumerate(result):
+            self.tableWidget.setRowCount(self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(elem)))
+        self.connection.close()
 
     def strees_open(self):
         self.settingfile = LoadStress()
@@ -56,19 +146,98 @@ class MainWearDesign(QWidget, MainDesignPy):
         year, ok_pressed = QInputDialog.getInt(self, "Год",
                                                 "Введите год", 2021, 2000, 2099, 1)
         if ok_pressed:
-            year, ok_pressed = QInputDialog.getItem(self, "Квартал",
+            quarter, ok_pressed = QInputDialog.getItem(self, "Квартал",
                                                     "Выберите Квартал", ('1', '2', '3', '4'), 1, False)
             if not ok_pressed:
                 win = QMessageBox()
                 win.setWindowTitle('Сообщение')
                 win.setText("Ошибка импорта")
                 win.exec()
+                return ''
         else:
             win = QMessageBox()
             win.setWindowTitle('Сообщение')
             win.setText("Ошибка импорта")
             win.exec()
-        print(fname)
+            return ''
+        collection350 = pd.read_excel(fname, sheet_name='до 350').to_csv(index=False).split('\r\n')[2:]
+        collection650 = pd.read_excel(fname, sheet_name='351-650').to_csv(index=False).split('\r\n')[2:]
+        collection1200 = pd.read_excel(fname, sheet_name='651-1200').to_csv(index=False).split('\r\n')[2:]
+        self.file = open("data/setting.txt", encoding="UTF-8")
+        data = self.file.readlines()
+        self.connection = mysql.connector.connect(
+            host=data[0][:-1],
+            user=data[1][:-1],
+            password=data[2][:-1],
+            database=data[3],
+        )
+        query = f'''Select stress_data From stress_table WHERE year = "{str(year)}" AND quarter = "{str(quarter)}"'''
+        cursor = self.connection.cursor(buffered=True)
+        cursor.execute(query)
+        stress_data = (cursor.fetchone())
+        #ПРОВЕРКА НА ГРУЗ
+        val_query = []
+        for element in collection350:
+            list_element = element.split(',')
+            pin = []
+            for index in range(len(list_element)):
+                if index == 9:
+                    pin.append(str(stress_data[0]))
+                    pin.append(list_element[index])
+                elif index == 12:
+                    if list_element[11] != '':
+                        pin.append(str(round(float(list_element[11]) / float(stress_data[0]), 5)))
+                        pin.append(list_element[index])
+                    else:
+                        pin.append(str(round(float(list_element[12]) / float(stress_data[0]), 5)))
+                        pin.append(list_element[index])
+                else:
+                    pin.append(list_element[index])
+            val_query.append(tuple(pin))
+        i = val_query.pop(-1)
+        for element in collection650:
+            list_element = element.split(',')
+            pin = []
+            for index in range(len(list_element)):
+                if index == 9:
+                    pin.append(str(stress_data[0]))
+                    pin.append(list_element[index])
+                elif index == 12:
+                    if list_element[11] != '':
+                        pin.append(str(round(float(list_element[11]) / float(stress_data[0]), 5)))
+                        pin.append(list_element[index])
+                    else:
+                        pin.append(str(round(float(list_element[12]) / float(stress_data[0]), 5)))
+                        pin.append(list_element[index])
+                else:
+                    pin.append(list_element[index])
+            val_query.append(tuple(pin))
+        i = val_query.pop(-1)
+        for element in collection1200:
+            list_element = element.split(',')
+            pin = []
+            for index in range(len(list_element)):
+                if index == 9:
+                    pin.append(str(stress_data[0]))
+                    pin.append(list_element[index])
+                elif index == 12:
+                    if list_element[11] != '':
+                        pin.append(str(round(float(list_element[11]) / float(stress_data[0]), 5)))
+                        pin.append(list_element[index])
+                    else:
+                        pin.append(str(round(float(list_element[12]) / float(stress_data[0]), 5)))
+                        pin.append(list_element[index])
+                else:
+                    pin.append(list_element[index])
+            val_query.append(tuple(pin))
+        i = val_query.pop(-1)
+        query = (f'''INSERT INTO `resault_iznos`(`ПЧ`, `Направление`, `Путь`, `Нач.крив Км`, `Нач.крив М`, `Длина кривой`, `Длина круговой`, `Длина переходн`, `R`, `Груз-нность`, `Пропущ. тоннаж`, `Скорость`, `Интенсивность БИ`, `РБИ мм/год`, `Износ средн`, `Износ макс`, `Дата последнего измерения`) values(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''')
+        for element in val_query:
+            cursor.execute(query, element)
+            self.connection.commit()
+        self.connection.close()
+        self.file.close()
+        self.update_table_widget()
 
     def export_xlxs(self):
         fname = QFileDialog.getSaveFileName(self,
@@ -77,12 +246,12 @@ class MainWearDesign(QWidget, MainDesignPy):
                              "All Files(*.*)")
         print()
 
+
 def main():
     app = QApplication(sys.argv)
     ex = MainWearDesign()
     ex.show()
     sys.exit(app.exec_())
-
 
 
 main()
